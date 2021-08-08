2b0403336e46c21ea0e78ac661ab4da4253fa551
Paulo Modesto <paulo.jmodesto@hotmail.com>
2021-08-07 23:19:34 -0300

    Other entities and second migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Data;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc
 {
